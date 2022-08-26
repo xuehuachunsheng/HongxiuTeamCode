b5b727b3a05ef92afe574a2d5ab4054dedb54118
ruanxiangwei <93184109+ruanxiangwei@users.noreply.github.com>
2022-08-26 22:28:48 +0800

    Delete 交错序列求和.py

--- 交错序列求和.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-result = 0
-for i in range(1,n+1,1):
-    if i%2==1:
-        result = result +i/(2*i-1)
-    else:
-        result = result -i/(2*i-1)
-print("{:.3f}".format(result))
