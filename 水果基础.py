e7b1852d8d94a3e62fb11f6b90d0c34aa7cdaca9
ruanxiangwei <93184109+ruanxiangwei@users.noreply.github.com>
2022-08-26 22:24:02 +0800

    Delete 水果基础.py

--- 水果基础.py
+++
@@ -1,8 +0,0 @@
-fruits = ["apple"," banana ","lemon"]
-print(fruits)
-print(fruits[0].title())#首字母大写
-print(fruits[1].rstrip())#删除末尾空格
-print(fruits[1].lstrip())#删除开头空格
-print(fruits[1].strip())#删除所有空格
-print(fruits[2].upper())#大写所有字母
-print(fruits[2].lower())#小写所有字母
