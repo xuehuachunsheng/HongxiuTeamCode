6a85e0602b96e864c2f6295a33c898e9cdac09ad
ruanxiangwei <93184109+ruanxiangwei@users.noreply.github.com>
2022-08-26 22:25:23 +0800

    Delete format的使用.py

--- format的使用.py
+++
@@ -1,8 +0,0 @@
-first_name = "ada"
-last_name = "lovelace"
-full_name = f"{first_name} {last_name}"
-print(full_name)
-print(f"Helll, {full_name.title()}!")
-full_name = "{} {}".format(first_name, last_name)
-print("\tfull_\nname")
-print("\n\thello")
