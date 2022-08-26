389b0dad6dfbeb2f97491eb69e722249a6f160f3
大鹅爱吃酸菜鱼 <93187671+DEAiFISH@users.noreply.github.com>
2022-08-26 23:08:56 +0800

    Add files via upload

---
+++ JavaWeb/ajaxDemo/src/main/java/web/servlet/AjaxServlet.java
@@ -0,0 +1,23 @@
+package web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/ajaxServlet")
+public class AjaxServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //1.响应数据
+        response.getWriter().write("hello ajax ~");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
---
+++ JavaWeb/ajaxDemo/src/main/java/web/servlet/SelectUserServlet.java
@@ -0,0 +1,29 @@
+package web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/selectUserServlet")
+public class SelectUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // 用户名
+        String username = request.getParameter("username");
+
+        //2.调用service查询User对象
+        boolean flag = true;
+
+        //3.响应标记
+        response.getWriter().write("" + flag);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
