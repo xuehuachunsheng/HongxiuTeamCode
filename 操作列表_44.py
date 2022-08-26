4dac81ead53542aabb4d0a867da6f31be5b37a8d
ruanxiangwei <93184109+ruanxiangwei@users.noreply.github.com>
2022-08-26 22:29:38 +0800

    Delete 操作列表_44.py

--- 操作列表_44.py
+++
@@ -1,8 +0,0 @@
-# magicians = ['alice', 'david', 'carolina']
-# for magician in magicians:
-#     print(magician.title() + " that was a grate trick!" + "\n")
-squares = []
-for value in range(1,11):
-    square = value ** 2
-    squares.append(square)
-print(squares)
