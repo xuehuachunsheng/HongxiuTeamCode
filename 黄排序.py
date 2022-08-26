5427cff1454c5d50d55b08f3cb2fc84f7f0e10e5
ruanxiangwei <93184109+ruanxiangwei@users.noreply.github.com>
2022-08-26 22:17:48 +0800

    Delete 黄排序.py

--- 黄排序.py
+++
@@ -1,9 +0,0 @@
-list=[5,45,36,85,42,75,99,44]
-for i in range(0,7):
-    for j in range (0,7-i):
-        if list[j] > list[j+1]:
-            n=list[j]
-            list[j]=list[j+1]
-            list[j+1]=n
-for i in range(0,8):
-    print(list[i])
