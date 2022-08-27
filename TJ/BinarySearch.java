3f116ef690f6c323b7a24412afab6d78d8960205
2142079441 <93320181+2142079441@users.noreply.github.com>
2022-08-27 10:48:20 +0800

    Update BinarySearch.java

--- TJ/BinarySearch.java
+++ TJ/BinarySearch.java
@@ -37,22 +37,3 @@ class Solution2 {
         return -1;
     }
 }
-
-//二分查找 时间复杂度为O(log2n)
-class Solution3 {
-    public int search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = (right - left) / 2 + left;
-            int num = nums[mid];
-            if (num == target) {
-                return mid;
-            } else if (num > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return -1;
-    }
-}
